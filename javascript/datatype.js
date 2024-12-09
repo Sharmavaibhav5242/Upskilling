ba5271fc5262d9ce71e13d2a8fa77d10bf9b7364
vaibhav <vaibhavsharma5242@gmail.com>
2024-12-09 21:55:15 +0530

    Strings in javascript

---
+++ javascript/datatype.js
@@ -0,0 +1,142 @@
+// function vaib (){
+    
+    // bom browser object model
+    // alert () ,  confirm () , prompt()
+    // window.alert("Thanks for visiting")
+    // let ans=window.confirm("Are you ok!")
+    // console.log(ans);
+    // let name=window.prompt("Enter your name")
+    // console.log(name)
+    // let num1=parseInt(window.prompt("Enter number 1"))
+    // let num2=parseInt(window.prompt("Enter nummber 2"))
+    // let ans=parseInt(window.alert((num1+num2)))
+
+    // parseint is to be used in the condition of sum of integer
+    // parsefloat to be used in the condition of decimal sum
+
+    // let inp=confirm("Do you like our website")
+    // if(inp==true)
+    // {
+    //     alert("Thankyou")
+    // }
+    // else
+    // {
+    //     alert("Sorry")
+//     // }
+// }
+// vaib()
+function ifelse(){
+
+    // let a=0;
+    // if(a>0)
+    // {
+    //     alert("Number is positive")
+    // }
+    // else if (a<0)
+    // {
+    //     alert("Number is  negative")
+    // }
+    // else
+    // {
+    //      alert("Number is zero")
+    // }
+    // let a=252;
+    // if(a%2==0)
+    // {
+    //     alert("The number is even ")
+    // }
+    // else
+    // {
+    //     alert("The number is odd")
+    // }
+    // ternary
+    // condition?"on true":"On false"
+    // let age= prompt("Enter your age")
+    // age>=18?alert("You can vote"):alert("You cannot vote");
+    // let a= confirm("Did you completed the course")
+    // let b= confirm("Do you have certificate")
+    // if (a==true && b==true)
+    // {
+    //     alert("Completed")
+    // }
+    // else
+    // {
+    //     alert("Not completed")
+    // }
+    // let price = parseInt(prompt("Enter price of the product"))
+    // let a = confirm("Do you have discount")
+    
+    // if(a==true)
+    // {
+    //     alert(price*0.9)
+       
+    // }
+    // else
+    // {
+    //     alert(price)
+    // }
+}
+ifelse();
+
+function homework(){
+// let marks = parseInt(prompt("Enter marks"))
+// if(marks>=33)
+// {
+//     alert("Pass")
+// }
+// else
+// {
+//     alert("Fail")
+// }
+// let  conf = confirm("Are you sure you want to delete this item")
+// if(conf==true)
+//     {
+    
+//         alert("Item deleted")
+//     } 
+//   else
+//   {
+//     alert("Cancelled")
+//   }
+// var i=1;
+// while(i<=10)
+// {
+//     alert(i)
+//     i++;
+// }
+// let input=parseInt(prompt("Enter number"))
+// do
+// {
+//     if(input>=10)
+//     {
+//         alert("ok")
+//     }
+//     else
+//     {
+//         alert("Enter again")
+//        input= parseInt(prompt("Enter number"))
+//     }
+
+// }while(input<10)
+// let days=parseInt(prompt("Enter day"))
+// switch(days)
+// {
+//     case 1: alert("Sunday")
+//     break;
+//     case 2: alert("Monday")
+//     break;
+//     case 3: alert("Tuesday") 
+//     break;
+//     case 4: alert("Wednesday") 
+//     break;
+//     case 5: alert("Thursday") 
+//     break;
+//     case 6: alert("Friday")
+//     break;
+//     case 7: alert("Saturday")
+//     break;
+//     default: alert("Error")
+//     break;
+// }
+}
+homework();
---
+++ javascript/string.js
@@ -0,0 +1,12 @@
+function vaib(){
+    let text="   This is a string      "
+    let text2="i am fromCybromOfficial"
+    // methods of strings
+    // document.write(text.length) returns the length of the string
+    // document.write(text.slice(2,10)) slice when given 2nd value gives n-1 value of its original value
+    // document.write(text2.slice(9,15))
+    // document.write(text.concat(text2)) add of string text and text2 
+    // document.write(text.trim(text)) cuts the white space and makes the string size smaller
+
+}
+vaib()
