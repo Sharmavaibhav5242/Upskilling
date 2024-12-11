12cc5dacc71eb0714190550bb0ec6197f4a03811
vaibhav <vaibhavsharma5242@gmail.com>
2024-12-11 22:41:52 +0530

    array in javascript

---
+++ javascript/array.js
@@ -0,0 +1,32 @@
+function arr(){
+    let fruits=["Apple","Mango","Banana","Grapes"]
+    // fruits.pop(); //removes last element (Grapes)
+    // fruits.pop(); //removes second lasr (Banana)
+
+    // let ans=fruits.push("Orange","Pineapple")
+    // document.write(fruits) //updated array
+    // fruits.shift() //remove first element
+    // document.write(fruits) //updated array
+
+    // fruits.unshift("Orange",12) //adds in starting
+    //document.write(fruits)
+
+    // let ans=fruits.length;
+    // document.write(ans) // 4
+
+    // let arr1=[3,5,2,4,9,7]
+    // let arr2=[4,5,6]
+
+    // let final= arr1.concat(arr2)
+    // document.write(final)
+ 
+    // let ans= arr1.sort() // gives in ascending order 
+    // document.write(ans)
+
+    // let ans=arr1.sort().reverse()
+    // document.write(ans) //gives in decending order
+
+
+
+}
+arr()
--- javascript/string.js
+++ javascript/string.js
@@ -1,12 +1,31 @@
 function vaib(){
-    let text="   This is a string      "
-    let text2="i am fromCybromOfficial"
-    // methods of strings
-    // document.write(text.length) returns the length of the string
-    // document.write(text.slice(2,10)) slice when given 2nd value gives n-1 value of its original value
-    // document.write(text2.slice(9,15))
-    // document.write(text.concat(text2)) add of string text and text2 
-    // document.write(text.trim(text)) cuts the white space and makes the string size smaller
-
+    // let text="The java is very fun Fun langauage "
+    // let text2="I Am fromCybromOfficial"
+    // methods of strings.
+    // document.write(text.length) returns the length of the string.
+    // document.write(text.slice(2,10)) slice when given 2nd value gives n-1 value of its original value.
+    // document.write(text2.slice(9,15)).
+    // document.write(text.concat(text2)) add of string text and text2 .
+    // document.write(text.trim(text)) cuts the white space and makes the string size smaller.
+    // document.write(text.toUpperCase(16)) simply makes fonts size to capital.
+    // document.write(text2.toLowerCase(24)) simply makes fonts to small size in whole string.
+    // let ans =text.replace(/fun/ig,"Hello")
+    // document.write(ans)
+    //  let ans = text.split("")
+    //  document.write(ans)
+    // let ans=`This is "Cybrom" and 'Sid' is faculty here`
+    // alert(ans)
+    // supports single quoted (' ') and double quotes ("")
+   
+     
 }
 vaib()
+
+// function okk(){
+//     alert("Hello world")
+// }
+function addi(){
+    let ans1= parseInt(prompt("Enter number one"))
+    let ans2= parseInt(prompt("Enter number two"))
+    alert(` the sum of ${ans1} and ${ans2} is ${ans1+ans2} `)
+}
